e4dc3954fa3aec49db8a5b38bbf4eb06897e5fa8
Song Yongseok <mexicoreanito@gmail.com>
2023-01-12 19:16:05 +0900

    feat: send a push notification via nfty.sh

--- src/mcdonalds-kr-happymeal.ts
+++ src/mcdonalds-kr-happymeal.ts
@@ -1,27 +1,67 @@
 import * as cheerio from 'cheerio'
 
+import { DateTime, Interval } from 'luxon'
+
 import axios from 'axios'
 
 const apiEndpoint = process.env.API_ENDPOINT as string
+const ntfyTopic = process.env.NTFY_TOPIC as string ?? 'mcdonalds-kr-happymeal'
 
 exports.main = async function(event: any, context: any) {
   try {
-      const resp = await axios.get(`${apiEndpoint}/kor/happymeal/listContent.do?page=1&searchStatus=I`)
-      const result = resp.data as Result
-      const summary = parseHtml(result.list[0].mo_kor_content)
-      console.log(JSON.stringify(summary))
+    console.log('Hola!')
+    const resp = await axios.get(`${apiEndpoint}/kor/happymeal/listContent.do?page=1&searchStatus=I`)
+    const content = (resp.data as Result).list?.[0]
+    if (content) {
+      if (isTodayValidToNotify(content)) {
+        const status = await reportThruNtfy(parseHtml(content.mo_kor_content))
+        console.log(status)
+      } else {
+        console.log('not today...')
+      }
+    }
   } catch (e) {
     console.error(e)
   }
 }
 
-const parseHtml = (html: string): Summary => {
+const isTodayValidToNotify = (content: Content): boolean => {
+  const zone = 'Asia/Seoul'
+  const now = DateTime.now().setZone(zone)
+  const start = DateTime.fromISO(content.open_time_start.substring(0, 10), { zone })
+  const end = DateTime.fromISO(content.open_time_end.substring(0, 10), { zone })
+
+  try {
+    const intervalStart = Interval.fromDateTimes(start, now).toDuration('hours').hours
+    const intervalEnd = Interval.fromDateTimes(now, end).toDuration('hours').hours
+    const aDay = 24
+    console.log(`intervalStart=${intervalStart} intervalEnd=${intervalEnd}`)
+    return ((typeof(intervalStart) === 'number' && intervalStart < aDay) || (typeof(intervalEnd) === 'number' && intervalEnd < aDay)) ? true : false
+  } catch (e) {
+    console.error(e)
+    return false
+  }
+}
+
+const reportThruNtfy = async (message: Message): Promise<number> => {
+  const result = await axios.post(`https://ntfy.sh/${ntfyTopic}`, message.body, 
+  { 
+    headers: {
+      Title: `This Month's Happy Meal Toys`,
+      Click: message.imgUrl,
+      Attach: message.imgUrl,
+    }
+  })
+  return result.status
+}
+
+const parseHtml = (html: string): Message => {
   const $ = cheerio.load(html)
   const imgUrl = `${apiEndpoint}${$('img')?.attr('src')?.trim() ?? ''}`
-  const description = $('p')?.text()?.trim() ?? ''
+  const body = $('p')?.text()?.trim() ?? ''
   return {
     imgUrl,
-    description
+    body,
   }
 }
 
@@ -47,7 +87,7 @@ interface Content {
   status: string
 }
 
-interface Summary {
+interface Message {
   imgUrl: string
-  description: string
+  body: string
 }
