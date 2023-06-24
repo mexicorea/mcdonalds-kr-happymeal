a70af462f18a345a264e2b28a2802d2fe7474e01
Song Yongseok <mexicoreanito@gmail.com>
2023-06-24 13:52:40 +0900

    chore: update Node.js runtime (from v14 to v18)

--- lib/lambda-construct.ts
+++ lib/lambda-construct.ts
@@ -3,6 +3,7 @@ import * as lambda from 'aws-cdk-lib/aws-lambda-nodejs'
 
 import { Construct } from 'constructs'
 import { Duration } from 'aws-cdk-lib'
+import { Runtime } from 'aws-cdk-lib/aws-lambda'
 
 export interface LambdaConstructDeps {
   vpc: cdk.aws_ec2.IVpc
@@ -18,6 +19,7 @@ export class LambdaConstruct extends Construct {
     this.handler = new lambda.NodejsFunction(this, id, {
       entry: `src/${id}.ts`,
       handler: 'main',
+      runtime: Runtime.NODEJS_18_X,
       timeout: Duration.seconds(10),
       environment: {
         API_ENDPOINT: 'https://www.mcdonalds.co.kr',
